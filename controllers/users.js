894b6cb7d7e1903a2a1e75a46e2c833ad2b2e2ec
kareen <kareenmilli3@gmail.com>
2023-09-10 21:57:20 +0300

    Add CHanges and fix git ignore

--- controllers/users.js
+++ controllers/users.js
@@ -4,14 +4,15 @@ const User = require('../models/User');
 exports.list= async(req,res,next)=>{
 
     const users = await User.find({})
+    
     res.send(users)
 };
 
 exports.create = async(req, res, next) => {
     const user = {
-        username: 'testin' + (new Date()),
+        username: 'testing' + (new Date()),
         email: Math.random() + 'testing@gmail.com' ,
-        password: '2342234r2'
+        password: '23dasfas42234r2'
     }
 
     return User.create(user)
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const {list, create}=require('../controllers/users');
 //routes
 router.route('/list').get(list);
 router.route('/').post(create);
+// router.route('/').update(create);
+// router.route('/').delete(create);
 
 
 module.exports =router;
