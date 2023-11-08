8f6d651ae47e41ff3e56ab56fd2552b6b362b3aa
wassim <wassim.lsabbagh@gmail.com>
2023-11-08 14:47:08 +0200

    REMOVE allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
-const allowedOrigins = [
-    'http://localhost:3000/',
-    'http://localhost:5000/',
-    'http://localhost:10000/',
-    'https://chatoo-cms.onrender.com/',
-    'https://chatoo.onrender.com/'
-]
+// const allowedOrigins = [
+//     'http://localhost:3000/',
+//     'http://localhost:5000/',
+//     'http://localhost:10000/',
+//     'https://chatoo-cms.onrender.com/',
+//     'https://chatoo.onrender.com/'
+// ]
 
-module.exports = allowedOrigins
+// module.exports = allowedOrigins
