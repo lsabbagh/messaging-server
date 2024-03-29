e5613ed6f81799fa85713cb9af416120e4cebc9e
wassim <wassim.lsabbagh@gmail.com>
2024-03-29 22:25:59 +0200

    remove describe

--- src/controllers/users/list/list.spec.ts
+++ src/controllers/users/list/list.spec.ts
@@ -1,18 +1,13 @@
 import {it, expect, describe} from 'vitest'
-import supertest from 'supertest'
-
-import {HttpStatusCode} from 'axios'
 
 
 // Ɖ✌: list users
-describe('✌', () => {
-    it('Unauthorized 😌',  () => {
-        // const response = await global.superTestApp
-        // .get('/api/users/list', {})
+it('Unauthorized 😌',  () => {
+    // const response = await global.superTestApp
+    // .get('/api/users/list', {})
 
-        // console.log('....', response.response)
+    // console.log('....', response.response)
 
-        // expect(response.status).toBe(HttpStatusCode.Unauthorized)
-        expect(true).toBe(true)
-    })
+    // expect(response.status).toBe(HttpStatusCode.Unauthorized)
+    expect(true).toBe(true)
 })
